43ef3ac018c76b61709b36dae513c817a6c842f5
plamencurso <plamencurso@gmail.com>
2015-11-08 14:00:53 +0000

    area51 is experimental...

---
+++ area51.php
@@ -0,0 +1,22 @@
+<?php
+include 'inc.php';
+myInit(__FILE__);
+#$code = '
+
+// HERE BE DRAGONS!!! (aparte de extraterestres :)
+
+// a ver como se hace un interpreter de Haskell en PHP
+
+// empezamos: que tenemos en Haskell? solo valores y funciones (de un solo argumento, aunque no lo parezca)
+// ay que tonto, tenemos tipos tambien :)
+
+
+
+
+
+#'; eval($code); echo gpp($code);
+?>    
+
+</body>
+</html>
+
