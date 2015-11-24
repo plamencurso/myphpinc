7afeadb5def9666487cd0381d4501c143c665921
plamencurso <plamencurso@gmail.com>
2015-11-24 00:46:37 +0000

    Por fin consigo el colorizador propio de PHP me faltaban los <?php ... ?> tags, sin ellos el colorizador - highlight_string() no quiere colorizar

--- arrays2.php
+++ arrays2.php
@@ -2,7 +2,7 @@
 $editing = true;  // do not include disqus
 include_once "inc.php"; // header
 myInit(__FILE__);
-#$code = '
+$code = '
 require_once "html.php";
 
 // Mis datos antiguos (del Viernes este ... :)
@@ -21,7 +21,7 @@ $ul = "<ul>Continentes:\n";    // acumulamos una lista para mostrar luego
 
 // transformamos los datos de arriba a un array cpc[continente][pais][ciudad] con valores de 1
 foreach ($continentes as $co) {
-    // ya que estamos aquí podemas hacer una lista para mostrar luego
+    // ya que estamos aquí podemos hacer una lista para mostrar luego
     $ul .= "<li>$co</li>\n  Paises:<ul>\n";                // abrimos una lista para los paises 
 
     foreach($paises[$co] as $pa) {
@@ -31,8 +31,6 @@ foreach ($continentes as $co) {
             $ul .= "      <li>$ci</li>\n";
             $cpc[$co][$pa][$ci] = 1;                // en este punto tenemos los 3 datos, se puede hacer de cualquier forma
             $lcpc[] = [$co, $pa, $ci];              // forma apta para table()
-            
-//            echo "\$cpc[\"$co\"][\"$pa\"][\"$ci\"] = " . $cpc[$co][$pa][$ci] . "<br/>";
         };
         $ul .= "    </ul>\n";                             // cerramos las listas abiertas                           
     };
@@ -46,11 +44,11 @@ echo $ul;
 // a ver si puedo mostrar $cpc con table() que necesita array de arrays
 echo table(["Continente", "País", "Ciudad"], $lcpc);   // si, puedo :)
 
-#'; eval($code); echo gpp($code);
+'; eval($code); highlight_string("<?php $code ?>");
+
 // getting my URL
 $yo = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["SCRIPT_NAME"];
 include "incf.php";
 
 if (!$editing) echo disqus_code($yo, __FILE__, "cursomm");
-?>    
-
+?> 
